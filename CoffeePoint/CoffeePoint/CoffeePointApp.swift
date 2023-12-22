1bef19fbce216c9eb9427a318cc80dc2a1d06661
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-12-22 14:04:42 +0300

    Create New Project

---
+++ CoffeePoint/CoffeePoint/CoffeePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeePointApp.swift
+//  CoffeePoint
+//
+//  Created by Ваня Науменко on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeePoint/CoffeePoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeePoint
+//
+//  Created by Ваня Науменко on 22.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
